2d91b35b4a798077ee4139cc885f552f78bc35ea
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-09-14 15:03:47 +0600

    beginning a new project

---
+++ front-end/src/routes/Route.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login/>,
+      },
+      {
+        path: "/signup",
+        element: <Signup/>,
+      },
+    ],
+  },
+]);
