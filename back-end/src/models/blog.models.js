f9c94dd7a47e60f069c4ff56403cfdd6670bb65a
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-09-18 12:52:29 +0600

    added utilites file od asynchandler and cloudinary

---
+++ back-end/src/models/blog.models.js
@@ -0,0 +1,12 @@
+/* import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const blogSchema = Schema(
+  {
+    blogTitle: {
+      type: String,
+      required: true,
+    },
+  },
+  { Timestamp: true }
+); */
---
+++ back-end/src/utils/asyncHandler.utils.js
@@ -0,0 +1,7 @@
+export const asyncHandler = async (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
